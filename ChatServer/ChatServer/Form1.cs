05932eefdd081f00c87e8b989ab1a135a9b87652
Gabriel Artigas <gaplogan@gmail.com>
2020-09-13 16:43:07 -0300

    2020.09.13.16.43

--- ChatServer/ChatServer/Form1.cs
+++ ChatServer/ChatServer/Form1.cs
@@ -1,20 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 
 namespace ChatServer
 {
     public partial class Form1 : Form
     {
+        private delegate void AtualizaStatusCallback(string strMensagem);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            if (txtIP.Text == string.Empty)
+            {
+                MessageBox.Show("Informe o endereço IP.");
+                txtIP.Focus();
+                return;
+            }
+
+            try
+            {
+
+                // Analisa o endereço IP do servidor informado no textbox
+                IPAddress enderecoIP = IPAddress.Parse(txtIP.Text);
+                int portaHost = (int)numPorta.Value;
+
+                // Cria uma nova instância do objeto ChatServidor
+                Servidor mainServidor = new Servidor(enderecoIP, portaHost);
+
+                // Vincula o tratamento de evento StatusChanged a mainServer_StatusChanged
+                Servidor.StatusChanged += new StatusChangedEventHandler(mainServidor_StatusChanged);
+
+                // Inicia o atendimento das conexões
+                mainServidor.IniciaAtendimento();
+
+                // Mostra que nos iniciamos o atendimento para conexões
+                listaLog.Items.Add("Monitorando as conexões...");
+                listaLog.SetSelected(listaLog.Items.Count - 1, true);
+            }
+            catch (Exception ex)
+            {
+                listaLog.Items.Add("Erro de conexão : " + ex.Message);
+                listaLog.SetSelected(listaLog.Items.Count - 1, true);
+            }
+        }
+
+        public void mainServidor_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // Chama o método que atualiza o formulário
+            this.Invoke(new AtualizaStatusCallback(this.AtualizaStatus), new object[] { e.EventMessage });
+        }
+
+        private void AtualizaStatus(string strMensagem)
+        {
+            // Atualiza o logo com mensagens
+            listaLog.Items.Add(strMensagem);
+            listaLog.SetSelected(listaLog.Items.Count - 1, true);
         }
     }
 }
---
+++ ChatServer/ChatServer/StatusChangedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChatServer
+{
+    // Trata os argumentos para o evento StatusChanged
+    public class StatusChangedEventArgs : EventArgs
+    {
+        // Estamos interessados na mensagem descrevendo o evento
+        private string EventMsg;
+
+        // Propriedade para retornar e definir um mensagem do evento
+        public string EventMessage
+        {
+            get { return EventMsg; }
+            set { EventMsg = value; }
+        }
+
+        // Construtor para definir a mensagem do evento
+        public StatusChangedEventArgs(string strEventMsg)
+        {
+            EventMsg = strEventMsg;
+        }
+    }
+}
